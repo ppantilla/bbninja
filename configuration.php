85ef9ae6492f2a4710430e70eaa59a38961cc8e6
Peter Pantilla <ppantilla@yahoo.com>
2017-07-07 23:52:06 +0800

    Signed-off-by: Peter Pantilla <ppantilla@yahoo.com>

--- configuration.php
+++ configuration.php
@@ -16,7 +16,7 @@ class JConfig {
 	public $db = 'heroku_eebb563a480856c';
 	public $dbprefix = 'prm_';
 	public $dbtype = 'mysqli';
-	public $debug = '0';
+	public $debug = '1';
 	public $debug_lang = '0';
 	public $display_offline_message = '1';
 	public $editor = 'tinymce';
