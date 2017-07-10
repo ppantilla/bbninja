37e6777d2a5311e90ba289cb30d1e648ccdbbe4d
Peter Pantilla <ppantilla@yahoo.com>
2017-07-10 12:44:15 +0800

    Signed-off-by: Peter Pantilla <ppantilla@yahoo.com>

--- test.php
+++ test.php
@@ -1,10 +1,5 @@
 <?
-    $connect=mysql_connect("us-cdbr-iron-east-03.cleardb.net","bcec7a14b74598","33573e82") or die("Unable to Connect");
-    mysql_select_db("heroku_eebb563a480856c") or die("Could not open the db");
-    $showtablequery="SHOW TABLES FROM dbname";
-    $query_result=mysql_query($showtablequery);
-    while($showtablerow = mysql_fetch_array($query_result))
-    {
-    echo $showtablerow[0]." ";
-    } 
+    error_reporting(E_ALL);
+ini_set("display_errors", 1);
+phpinfo();
     ?>
