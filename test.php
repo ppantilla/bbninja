2a0b8fc88beb0961bdf346d81297628cc05e46cd
Peter Pantilla <ppantilla@yahoo.com>
2017-07-10 16:00:02 +0800

    Signed-off-by: Peter Pantilla <ppantilla@yahoo.com>

--- test.php
+++ test.php
@@ -1,6 +1,16 @@
 <?
     error_reporting(E_ALL);
 ini_set("display_errors", 1);
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
 $link = mysqli_connect('localhost', 'my_user', 'my_password', 'my_db');
 
 if (!$link) {
