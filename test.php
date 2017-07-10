5236a92ba5f0cacc913ce423489d0faeb336f0be
Peter Pantilla <ppantilla@yahoo.com>
2017-07-10 13:06:11 +0800

    Signed-off-by: Peter Pantilla <ppantilla@yahoo.com>

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?
     error_reporting(E_ALL);
 ini_set("display_errors", 1);
-phpinfo();
+$link = mysqli_connect('localhost', 'my_user', 'my_password', 'my_db');
+
+if (!$link) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+echo 'Connected... ' . mysqli_get_host_info($link) . "\n";
     ?>
