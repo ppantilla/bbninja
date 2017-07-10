ef6552d893aa1db5d366b21eff4f51619f39e356
Peter Pantilla <ppantilla@yahoo.com>
2017-07-10 20:09:36 +0800

    Signed-off-by: Peter Pantilla <ppantilla@yahoo.com>

--- test.php
+++ test.php
@@ -2,18 +2,5 @@
     error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
-if (!$conn) {
-    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
-}
-
-echo 'Connected... ' . mysqli_get_host_info($link) . "\n";
+echo realpath('../a.php');
     ?>
